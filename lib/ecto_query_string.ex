f49f5ed5b44497439661c7db3f5a2329d031db70
Micah Cooper <micah+github@mrmicahcooper.com>
2018-03-19 22:15:24 -0500

    Initial commit

---
+++ lib/ecto_query_string.ex
@@ -0,0 +1,18 @@
+defmodule EctoQueryString do
+  @moduledoc """
+  Documentation for EctoQueryString.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoQueryString.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_query_string_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoQueryStringTest do
+  use ExUnit.Case
+  doctest EctoQueryString
+
+  test "greets the world" do
+    assert EctoQueryString.hello() == :world
+  end
+end
