cc12ae278dc4cf79424076a81de9d773e869b357
Micah Cooper <micah+github@mrmicahcooper.com>
2018-04-08 20:54:56 -0500

    Use queryable and selectable

--- lib/ecto_query_string.ex
+++ lib/ecto_query_string.ex
@@ -1,40 +1,50 @@
 defmodule EctoQueryString do
   import Ecto.Query
-  #/"!@$#*()-_;:',.~[]"
+  # /"!@$#*()-_;:',.~[]"
 
-  def string_query(query, ""), do: string_query(query, %{})
+  def query(query, ""), do: query(query, %{})
+  def query(query, nil), do: query(query, %{})
 
-  def string_query(query, nil), do: string_query(query, %{})
-
-  def string_query(query, %{} = params) do
+  def query(query, %{} = params) do
     Enum.reduce(params, query, &dynamic_segment/2)
   end
 
-  def string_query(query, querystring) when is_binary(querystring) do
+  def query(query, querystring) when is_binary(querystring) do
     params = URI.decode_query(querystring)
-    string_query(query, params)
+    query(query, params)
   end
 
-  defp schema_fields(fields_string, query) when is_binary(fields_string) do
-    fields_string
-    |> String.split(",")
-    |> schema_fields(query)
+  def queryable?(field, query) do
+    fields = schema_fields(query)
+
+    if(Enum.find(fields, &Kernel.==(&1, field))) do
+      String.to_atom(field)
+    end
   end
 
-  defp schema_fields(fields, query) when is_list(fields) do
-    schema_fields =
-      query.from
-      |> elem(1)
-      |> struct()
-      |> Map.from_struct()
-      |> Map.keys()
-      |> Enum.map(&to_string/1)
+  def selectable(fields, query) do
+    fields =
+      fields
+      |> String.split(",")
+      |> Enum.map(&String.trim/1)
 
-    for field <- fields, field in schema_fields, do: String.to_atom(field)
+    schema_fields = schema_fields(query)
+
+    fields = for field <- fields, field && field in schema_fields, do: field
+    Enum.map(fields, &String.to_atom/1)
+  end
+
+  def schema_fields(query) do
+    query.from
+    |> elem(1)
+    |> Map.from_struct()
+    |> Map.drop([:__meta__])
+    |> Map.keys()
+    |> Enum.map(&to_string/1)
   end
 
   defp dynamic_segment({"@", value}, acc) do
-    attrs = schema_fields(value, acc)
+    attrs = selectable(value, acc)
     from(acc, select: ^attrs)
   end
 
@@ -49,7 +59,7 @@ defmodule EctoQueryString do
   end
 
   defp dynamic_segment({"..." <> key, value}, acc) do
-    if new_key = schema_fields(key, acc) |> List.first() do
+    if new_key = queryable?(key, acc) do
       dynamic =
         case String.split(value, ":") do
           [".", "."] -> acc
@@ -66,14 +76,14 @@ defmodule EctoQueryString do
   end
 
   defp dynamic_segment({"$asc", values}, acc) do
-    order_values = schema_fields(values, acc)
+    order_values = selectable(values, acc)
     from(acc, order_by: ^order_values)
   end
 
   defp dynamic_segment({"$desc", values}, acc) do
     order_values =
       values
-      |> schema_fields(acc)
+      |> selectable(acc)
       |> Enum.map(fn value -> {:desc, value} end)
 
     from(acc, order_by: ^order_values)
@@ -81,21 +91,21 @@ defmodule EctoQueryString do
 
   defp dynamic_segment({"i~" <> key, value}, acc) do
     value = String.replace(value, "*", "%")
-    [new_key] = schema_fields(key, acc)
+    new_key = queryable?(key, acc)
     dynamic = dynamic([q], ilike(field(q, ^new_key), ^value))
     from(acc, where: ^dynamic)
   end
 
   defp dynamic_segment({"~" <> key, value}, acc) do
     value = String.replace(value, "*", "%")
-    [new_key] = schema_fields(key, acc)
+    new_key = queryable?(key, acc)
     dynamic = dynamic([q], like(field(q, ^new_key), ^value))
     from(acc, where: ^dynamic)
   end
 
   defp dynamic_segment({"!" <> key, value}, acc) do
     value = String.split(value, ",")
-    new_key = schema_fields(key, acc) |> List.first()
+    new_key = queryable?(key, acc)
 
     case {new_key, value} do
       {nil, _} ->
@@ -114,7 +124,7 @@ defmodule EctoQueryString do
 
   defp dynamic_segment({"/!" <> key, value}, acc) do
     value = String.split(value, ",")
-    new_key = schema_fields(key, acc) |> List.first()
+    new_key = queryable?(key, acc)
 
     case {new_key, value} do
       {nil, _} ->
@@ -133,7 +143,7 @@ defmodule EctoQueryString do
 
   defp dynamic_segment({"/" <> key, value}, acc) do
     value = String.split(value, ",")
-    new_key = schema_fields(key, acc) |> List.first()
+    new_key = queryable?(key, acc)
 
     case {new_key, value} do
       {nil, _} ->
@@ -152,7 +162,7 @@ defmodule EctoQueryString do
 
   defp dynamic_segment({key, value}, acc) do
     value = String.split(value, ",")
-    new_key = schema_fields(key, acc) |> List.first()
+    new_key = queryable?(key, acc)
 
     case {new_key, value} do
       {nil, _} ->
--- test/ecto_query_string_test.exs
+++ test/ecto_query_string_test.exs
@@ -1,6 +1,6 @@
 defmodule EctoQueryStringTest do
   use ExUnit.Case, async: true
-  import EctoQueryString, only: [string_query: 2]
+  import EctoQueryString
   import Ecto.Query
 
   defmodule User do
@@ -14,6 +14,34 @@ defmodule EctoQueryStringTest do
       field(:password_digest, :string)
       timestamps()
     end
+
+    def selectable, do: ~w[username email]a
+    def queryable, do: ~w[username email]a
+
+    def not_queryable, do: ~w[username email]a
+    def not_selectable, do: ~w[username email]a
+  end
+
+  defmodule Foo do
+    use Ecto.Schema
+
+    schema "foos" do
+      field(:foo, :integer)
+      field(:title, :string)
+      field(:description, :string)
+    end
+  end
+
+  defmodule Bar do
+    use Ecto.Schema
+
+    schema "bars" do
+      field(:bar, :integer)
+      field(:title, :string)
+      field(:description, :string)
+    end
+
+    def queryable_fields, do: [:bar, :title]
   end
 
   defp assert_queries_match(query1, query2) do
@@ -28,6 +56,18 @@ defmodule EctoQueryStringTest do
   defp cleaned(nil), do: nil
   defp cleaned(maps), do: Enum.map(maps, &cleaned/1)
 
+  test ".fields gets the fields from a query.from" do
+    query = from(f in Foo)
+    assert schema_fields(query) == ~w[description foo id title]
+  end
+
+  test ".queryable returns the field if its in the query's schema" do
+    query = from(f in Foo)
+    assert queryable?("title", query) == :title
+    assert queryable?("description", query) == :description
+    assert queryable?("bar", query) == nil
+  end
+
   setup do
     query = from(user in User)
     {:ok, %{query: query}}
@@ -35,21 +75,21 @@ defmodule EctoQueryStringTest do
 
   test "all", %{query: query} do
     querystring = ""
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User)
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE Key = value", %{query: query} do
     querystring = "foo=bar&username=foo"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: user.username == ^"foo")
     assert_queries_match(string_query, expected_query)
   end
 
   test("WHERE key IN value", %{query: query}) do
     querystring = "email=user@clank.us,micah@clank.us&username=mrmicahcooper"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
 
     expected_query =
       from(
@@ -63,147 +103,147 @@ defmodule EctoQueryStringTest do
 
   test "WHERE Key != value", %{query: query} do
     querystring = "!username=mrmicahcooper"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: user.username != ^"mrmicahcooper")
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE key LIKE value%", %{query: query} do
     querystring = "~email=user*"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: like(user.email, ^"user%"))
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE key LIKE %value", %{query: query} do
     querystring = "~email=*clank.us"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: like(user.email, ^"%clank.us"))
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE key LIKE %value%", %{query: query} do
     querystring = "~email=*clank.us*"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: like(user.email, ^"%clank.us%"))
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE key ILIKE value%", %{query: query} do
     querystring = "i~email=*clank.us*"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: ilike(user.email, ^"%clank.us%"))
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE key ILIKE %value", %{query: query} do
     querystring = "i~email=*clank.us"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: ilike(user.email, ^"%clank.us"))
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE key ILIKE %value%", %{query: query} do
     querystring = "i~email=*@*"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: ilike(user.email, ^"%@%"))
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE key > value", %{query: query} do
     querystring = "...age=30:."
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: user.age > ^"30")
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE key < value", %{query: query} do
     querystring = "...age=.:100"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: user.age < ^"100")
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE key < max and key > min ", %{query: query} do
     querystring = "...age=100:200"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: user.age > ^"100" and user.age < ^"200")
     assert_queries_match(string_query, expected_query)
   end
 
   test "LIMIT max", %{query: query} do
     querystring = "...=.:2"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, limit: ^"2")
     assert_queries_match(string_query, expected_query)
   end
 
   test "OFFSET min", %{query: query} do
     querystring = "...=2:."
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, offset: ^"2")
     assert_queries_match(string_query, expected_query)
   end
 
   test "OFFSET min and LIMIT max", %{query: query} do
     querystring = "...=100:150"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, limit: ^"150", offset: ^"100")
     assert_queries_match(string_query, expected_query)
   end
 
   test "WHERE key NOT IN value", %{query: query} do
     querystring = "!email=a@b.co,c@d.co"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, where: user.email not in ^["a@b.co", "c@d.co"])
     assert_queries_match(string_query, expected_query)
   end
 
   test "OR WHERE key = value", %{query: query} do
     querystring = "/email=a@b.co"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, or_where: user.email == ^"a@b.co")
     assert_queries_match(string_query, expected_query)
   end
 
   test "OR WHERE key != value", %{query: query} do
     querystring = "/!email=a@b.co"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, or_where: user.email != ^"a@b.co")
     assert_queries_match(string_query, expected_query)
   end
 
   test "OR WHERE key NOT IN value", %{query: query} do
     querystring = "/!email=a@b.co,c@d.co"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, or_where: user.email not in ^["a@b.co", "c@d.co"])
     assert_queries_match(string_query, expected_query)
   end
 
   test "OR WHERE key IN value", %{query: query} do
     querystring = "/email=a@b.co,c@d.co"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, or_where: user.email in ^["a@b.co", "c@d.co"])
     assert_queries_match(string_query, expected_query)
   end
 
   test "SELECT values", %{query: query} do
     querystring = "@=username,email"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, select: ^[:username, :email])
     assert_queries_match(string_query, expected_query)
   end
 
   test "ORDER_BY values ASC", %{query: query} do
     querystring = "$asc=username,email"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, order_by: ^[asc: :username, asc: :email])
     assert_queries_match(string_query, expected_query)
   end
 
   test "ORDER_BY values DESC", %{query: query} do
     querystring = "$desc=username"
-    string_query = string_query(query, querystring)
+    string_query = query(query, querystring)
     expected_query = from(user in User, order_by: ^[desc: :username])
     assert_queries_match(string_query, expected_query)
   end
