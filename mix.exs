4402655b833040b10046b9b2e6a9a3e6b5e3e4d8
Micah Cooper <micah+github@mrmicahcooper.com>
2019-02-13 21:52:17 -0600

    Update deps

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule EctoQueryString.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 2.2.9"}
+      {:ecto, "~> 3.0"}
     ]
   end
 end
