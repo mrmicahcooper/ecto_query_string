6efdbfb7ad0f894601919191359b236ee0959f0f
Micah Cooper <micah+github@mrmicahcooper.com>
2019-10-28 22:24:51 -0400

    Update example play task/populate

--- example/.iex.exs
+++ example/.iex.exs
@@ -3,3 +3,9 @@ import EctoQueryString
 alias Example.Repo
 alias Example.{User, Project, Task}
 
+query = from(user in User)
+
+querystring =
+  "select=id,username,age,projects.description,tasks.name,tasks.id,tasks.project_id,projects.id,projects.user_id&limit=1"
+
+ecto_query_string = EctoQueryString.query(query, querystring)
--- example/lib/tasks/populate.ex
+++ example/lib/tasks/populate.ex
@@ -4,28 +4,32 @@ defmodule Mix.Tasks.Example.Populate do
   def run(_) do
     Mix.Task.run("app.start")
 
-    user = %Example.User{ email: "foo@example.com", username: "foobar", age: 99 }
-           |> Example.Repo.insert!()
+    user =
+      %Example.User{email: "foo@example.com", username: "foobar", age: 99}
+      |> Example.Repo.insert!()
 
-    user2 = %Example.User{ email: "bar@example.com", username: "barbaz", age: 45 }
-            |> Example.Repo.insert!()
+    user2 =
+      %Example.User{email: "bar@example.com", username: "barbaz", age: 45}
+      |> Example.Repo.insert!()
 
-    project = %Example.Project{name: "Work", description: "do work", user_id: user.id}
-              |> Example.Repo.insert!()
+    project =
+      %Example.Project{name: "Work", description: "do work", user_id: user.id}
+      |> Example.Repo.insert!()
 
-    project2 = %Example.Project{name: "Work Harder", description: "do work harder", user_id: user2.id}
-               |> Example.Repo.insert!()
+    project2 =
+      %Example.Project{name: "Work Harder", description: "do work harder", user_id: user2.id}
+      |> Example.Repo.insert!()
 
     %Example.Task{name: "task1", description: "task one", project_id: project.id}
-           |> Example.Repo.insert!()
+    |> Example.Repo.insert!()
 
     %Example.Task{name: "task2", description: "task one", project_id: project.id}
-           |> Example.Repo.insert!()
+    |> Example.Repo.insert!()
 
     %Example.Task{name: "task3", description: "task one", project_id: project.id}
-           |> Example.Repo.insert!()
+    |> Example.Repo.insert!()
 
     %Example.Task{name: "task4", description: "task one", project_id: project2.id}
-           |> Example.Repo.insert!()
+    |> Example.Repo.insert!()
   end
 end
