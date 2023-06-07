ba67b48923de297d3a36d4053930849772618388
Micah Cooper <micah+github@mrmicahcooper.com>
2023-06-07 09:50:34 -0500

    0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoQueryString.MixProject do
   def project do
     [
       app: :ecto_query_string,
-      version: "0.1.2",
+      version: "0.2.0",
       description: "Easy querying with ecto and query string params",
       elixir: ">= 1.9.1",
       name: "EctoQueryString",
